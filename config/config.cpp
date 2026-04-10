0b758f3fa813600afcdef98f3885479372407870
soramon0 <23435312+soramon0@users.noreply.github.com>
2026-04-10 16:43:00 +0100

    refactor: enhance README with additional nginx configuration examples and usage scenarios for better user guidance

---
+++ config/config.cpp
@@ -0,0 +1,61 @@
+// # global config
+#include <map>
+#include <string>
+#include <vector>
+
+struct ReturnDir {
+  int code;        // optional, default to temporary redirect 302
+  std::string url; // redirect url
+};
+
+struct SharedConfig {
+  std::string root;
+  std::vector<std::string> index;
+  // directory listing if path matches and has a trailing /
+  // autoindex off by default. index has precedence over autoindex.
+  bool autoindex;
+  // collect `error_page 500 502 503 504 ./50x.html` into a map
+  // {500: "./50x.html", 502: "./50x.html", 503: "./50x.html", 504:"./50x.html"}
+  std::map<int, std::string> error_page;
+  size_t client_max_body_size;
+  // empty string is off but if we have a string then it's the path where to
+  // save the access log
+  std::string access_log_path;
+  std::map<std::string, std::vector<std::string>> types;
+  // where to save uploads of locations with POST method
+  std::string upload_store; // defaults to /tmp/webserve
+  //  [0]([0].py [1]/usr/bin/python3)
+  std::vector<std::pair<std::string, std::string>> cgi_pass;
+};
+
+struct Location {
+  std::string path;
+  std::vector<std::string> methods; // defaults to GET if not defined
+  ReturnDir *return_rule;
+
+  // copy from parent (server) and update if needed to match location
+  SharedConfig *shared_config;
+};
+
+struct Server {
+  // std::string server_name; virtual hosts
+  int port;
+  std::string interface;
+  std::vector<Location> locations;
+  ReturnDir *return_rule;
+
+  // copy from parent (http) and update if needed to match server
+  SharedConfig *shared_config;
+};
+
+//  shared config between all servers
+struct Http {
+  std::vector<Server> servers;
+  SharedConfig *shared_config;
+};
+
+//  global config
+struct Config {
+  //   Logger *l
+  Http http;
+};
