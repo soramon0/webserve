ad166cf62fdfe2a9d0d6c1f8aca0a650f07929ca
teathas <asmaber722@gmail.com>
2026-06-27 14:52:34 +0100

    hm

--- src/cgi/CgiManager.cpp
+++ src/cgi/CgiManager.cpp
@@ -1,11 +1,44 @@
 #include "CgiManager.hpp"
+#include <sstream>
 
-std::string	CgiManager::pipeReady(int pipe_fd)
+CgiManager::CgiManager(int& epoll_fd) : _epoll_fd(epoll_fd) {}
+
+std::map<std::string, std::string> CgiManager::buildEnv(const Request& req, const SharedConfig& conf)
 {
+	std::map<std::string, std::string> env;
 
+	env["REQUEST_METHOD"] = req.getMethod();
+	env["CONTENT_TYPE"] = req.getContentType();
+	env["QUERY_STRING"] = req.getQuery();
+	env["SCRIPT_FILENAME"] = conf.root +  req.getPath();
+
+	std::ostringstream ss;
+	ss << req.getBody().size();
+	env["CONTENT_LENGTH"] = ss.str();
+
+	return (env);
 }
 
-bool		CgiManager::isCgiFd(int fd)
+std::string CgiManager::getExtension(const std::string& path)
 {
+	size_t pos = path.find_last_of('.');
+	if (pos == std::string::npos)
+		return ("");
+	std::string extension = path.substr(pos);
+	return (extension);
+}
 
+int CgiManager::handleRequest(const Request& req, const SharedConfig& conf, int client_fd)
+{
+	std::map<std::string, std::string> env = buildEnv(req, conf);
+	std::string interpreter = conf.cgi_pass[getExtension(req.getPath())];
+
+	CgiHandler cgi(req.getPath(), interpreter, req.getBody(), env);
+	return (cgi.start());
+}
+
+
+bool	CgiManager::isCgiFd(int fd)
+{
+	return (_cgiHandlers.count(fd));
 }
--- src/cgi/CgiManager.hpp
+++ src/cgi/CgiManager.hpp
@@ -1,6 +1,7 @@
 #ifndef CGIMANAGER_HPP
 #define CGIMANAGER_HPP
 
+#include "../config/shared_config.hpp"
 #include "Request.hpp"
 #include "CgiHandler.hpp"
 #include <map>
@@ -8,8 +9,20 @@
 class CgiManager
 {
 private:
-	std::map<int, CgiHandler*>	_cgiHandlers;
-	std::map<int, int>			_cgiToClient;
+	std::map<int, CgiHandler*>		_cgiHandlers;
+	std::map<int, int>				_cgiToClient;
+	int&							_epoll_fd;
+
+	
+	std::map<std::string, std::string>	buildEnv(const Request& req, const SharedConfig& conf);
+	std::string 						getExtension(const std::string& path);
+
+public:
+	CgiManager(int& epoll_fd);
+
+	int									handleRequest(const Request& req, const SharedConfig& conf, int client_fd);
+	std::string							pipeReady(int pipe_fd);
+	bool								isCgiFd(int fd);
 };
 
 #endif
