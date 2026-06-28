1b56824428dabf37bc7c13ff69df8e7e146b7f92
Karim Laayoun <klaayoun@c4r4p9.1337.ma>
2026-06-28 18:06:46 +0100

    tests: add more tests for parsing request-line

--- tests/src/request/request-line.test.cpp
+++ tests/src/request/request-line.test.cpp
@@ -21,16 +21,127 @@ TEST_CASE("FSM parses complete HTTP request line across arbitrary chunks") {
   CHECK(req->version.toString() == "HTTP/1.1");
 }
 
-// TEST_CASE("FSM HTTP version") {
-//   FSM fsm;
-//   std::string input = "GET /api/products HTTP/1.1 \r\n";
-//
-//   REQUIRE(fsm.feedChunk(input.c_str(), input.length()) == true);
-//   REQUIRE(fsm.status.isDone());
-//
-//   HttpRequest *req = fsm.getRequest();
-//   CHECK(req->status == HttpStatus::OK);
-//   CHECK(req->method.toString() == "GET");
-//   CHECK(std::string(req->uri.data(), req->uri.length()) == "/api/products");
-//   CHECK(req->version.toString() == "HTTP/1.1");
-// }
+TEST_CASE("FSM correctly extracts path from Absolute URI format") {
+  FSM fsm;
+  std::string input = "GET https://example.com/api/products HTTP/1.1\r\n";
+
+  CHECK(fsm.feedChunk(input.data(), input.length()));
+  REQUIRE(fsm.status.isPending());
+
+  HttpRequest *req = fsm.getRequest();
+  REQUIRE(req != nullptr);
+  CHECK(req->status == HttpStatus::OK);
+  CHECK(req->method.toString() == "GET");
+
+  CHECK(std::string(req->uri.data(), req->uri.length()) == "/api/products");
+  CHECK(req->version.toString() == "HTTP/1.1");
+}
+
+TEST_CASE("FSM parses absolute URI split across chunks") {
+  FSM fsm;
+
+  std::vector<std::string> chunks = {"GET htt", "ps://examp",
+                                     "le.com/api/products HT", "TP/1.1\r\n"};
+  for (const auto &chunk : chunks) {
+    CHECK(fsm.feedChunk(chunk.data(), chunk.length()));
+    REQUIRE(fsm.status.isPending());
+  }
+
+  HttpRequest *req = fsm.getRequest();
+  REQUIRE(req != nullptr);
+  CHECK(req->status == HttpStatus::OK);
+  CHECK(std::string(req->uri.data(), req->uri.length()) == "/api/products");
+}
+
+TEST_CASE("FSM handles absolute URI with missing trailing path") {
+  FSM fsm;
+  std::string input = "GET http://example.com HTTP/1.1\r\n";
+
+  CHECK(fsm.feedChunk(input.data(), input.length()));
+  REQUIRE(fsm.status.isPending());
+
+  HttpRequest *req = fsm.getRequest();
+  REQUIRE(req != nullptr);
+  CHECK(std::string(req->uri.data(), req->uri.length()) == "/");
+}
+
+TEST_CASE("FSM rejects missing uri") {
+  FSM fsm;
+  std::string bad_input = "GET\r\n";
+  size_t error_index = bad_input.find('\r');
+
+  for (size_t i = 0; i < 3; ++i) {
+    CHECK(fsm.feedChunk(&bad_input[i], 1));
+    REQUIRE(fsm.status.isPending());
+  }
+
+  CHECK(!fsm.feedChunk(&bad_input[error_index], 1));
+  CHECK(fsm.status.isMalformed());
+
+  HttpRequest *req = fsm.getRequest();
+  REQUIRE(req != nullptr);
+  CHECK(req->status == HttpStatus::BAD_REQUEST);
+}
+
+TEST_CASE("FSM rejects missing HTTP version") {
+  FSM fsm;
+  std::string bad_input = "GET /api/products\r\n";
+  size_t error_index = bad_input.find('\r');
+
+  for (size_t i = 0; i < error_index; ++i) {
+    CHECK(fsm.feedChunk(&bad_input[i], 1));
+    REQUIRE(fsm.status.isPending());
+  }
+
+  CHECK(!fsm.feedChunk(&bad_input[error_index], 1));
+  CHECK(fsm.status.isMalformed());
+
+  HttpRequest *req = fsm.getRequest();
+  REQUIRE(req != nullptr);
+  CHECK(req->status == HttpStatus::BAD_REQUEST);
+}
+
+TEST_CASE("FSM correctly rejects various malformed request lines") {
+  FSM fsm;
+  std::string bad_input;
+
+  SUBCASE("Missing space between method and URI") {
+    bad_input = "GET/api/productsHTTP/1.1\r\n";
+    CHECK(!fsm.feedChunk(bad_input.data(), bad_input.length()));
+  }
+
+  SUBCASE("Tabs instead of spaces") {
+    bad_input = "GET\t/api/products\tHTTP/1.1\r\n";
+    CHECK(!fsm.feedChunk(bad_input.data(), bad_input.length()));
+  }
+
+  SUBCASE("Multiple spaces between method and URI") {
+    bad_input = "GET  /api/products HTTP/1.1\r\n";
+    CHECK(!fsm.feedChunk(bad_input.data(), bad_input.length()));
+  }
+
+  SUBCASE("Malformed HTTP version format") {
+    bad_input = "GET /api/products http/1.1\r\n";
+    CHECK(!fsm.feedChunk(bad_input.data(), bad_input.length()));
+  }
+
+  SUBCASE("Empty request-line") {
+    bad_input = "";
+    CHECK(!fsm.feedChunk(bad_input.data(), bad_input.length()));
+  }
+
+  SUBCASE("Unsupported URI Scheme") {
+    bad_input = "GET ftp://example.com/api/products HTTP/1.1\r\n";
+    CHECK(!fsm.feedChunk(bad_input.data(), bad_input.length()));
+  }
+
+  SUBCASE("Missing domain name in absolute URI") {
+    bad_input = "GET https:// HTTP/1.1\r\n";
+    CHECK(!fsm.feedChunk(bad_input.data(), bad_input.length()));
+  }
+
+  REQUIRE(fsm.status.isMalformed());
+  HttpRequest *req = fsm.getRequest();
+  REQUIRE(req != nullptr);
+  CHECK(req->status == HttpStatus::BAD_REQUEST);
+}
