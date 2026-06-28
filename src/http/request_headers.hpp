60a89229e663c810af914a96170908fdf2df3491
soramon0 <23435312+soramon0@users.noreply.github.com>
2026-06-28 22:20:46 +0100

    feat: multi key headers class

--- src/http/request_headers.hpp
+++ src/http/request_headers.hpp
@@ -2,38 +2,59 @@
 #include "lib/string_view.hpp"
 #include <map>
 
-#ifndef REQUEST_HEADERS_
-#define REQUEST_HEADERS_
-
-typedef std::map<StringView, StringView> HStore;
-
-#endif // ! REQUEST_HEADERS_
-
 class Headers {
 private:
+  typedef std::multimap<StringView, StringView> HStore;
   HStore store;
 
 public:
+  typedef std::pair<HStore::const_iterator, HStore::const_iterator> Range;
+
   Headers() {};
 
-  StringView get(const StringView &key) const {
+  /**
+   * @brief Retrieves the first value associated with a header key.
+   * @param key The HTTP header key to search for. MUST BE STRICTLY LOWERCASE.
+   * @return A const pointer to the StringView containing the value,
+   * or NULL if the header does not exist.
+   */
+  const StringView *get(const StringView &key) const {
     HStore::const_iterator it = store.find(key);
 
     if (it != store.end()) {
-      return it->second;
+      return &(it->second);
     }
 
-    return StringView();
+    return NULL;
   }
 
+  /**
+   * @brief Retrieves all values associated with a header key.
+   * * Useful for headers like 'Accept' or 'Set-Cookie' that may appear multiple
+   * times. If the key does not exist, range.first will equal range.second.
+   * * @param key The HTTP header key to search for. MUST BE STRICTLY LOWERCASE.
+   * @return A Range pair of const_iterators to safely loop through the values.
+   */
+  Range get_all(const StringView &key) const { return store.equal_range(key); }
+
+  /**
+   * @brief Inserts a new header key-value pair.
+   * If the key already exists, this does not overwrite it; it appends the new
+   * value to the internal multimap alongside the existing ones.
+   * @param key The header key. The caller MUST lowercase the buffer it points
+   * to.
+   * @param value The header value.
+   */
   void set(const StringView &key, const StringView &value) {
-    std::pair<HStore::iterator, bool> result =
-        store.insert(std::make_pair(key, value));
-
-    if (!result.second) {
-      // key already exists
-      // TODO: may need to appened value
-      result.first->second = value;
-    }
+    store.insert(std::make_pair(key, value));
   }
-}
+
+  /**
+   * @brief Checks if a specific header exists.
+   * @param key The HTTP header key to search for. MUST BE STRICTLY LOWERCASE.
+   * @return true if the header is found, false otherwise.
+   */
+  bool has(const StringView &key) const {
+    return store.find(key) != store.end();
+  }
+};
