eae85d5c8ab793ce06ed3e96346a7b83b6a05a8d
Karim Laayoun <klaayoun@c4r4p3.1337.ma>
2026-07-01 15:18:34 +0100

    feat: update string view to support comparisons with char *

--- src/lib/string_view.hpp
+++ src/lib/string_view.hpp
@@ -17,14 +17,47 @@ public:
   const char *data() const { return buf; }
   std::size_t length() const { return len; }
   bool empty() const { return len == 0; }
+  void clear() {
+    buf = NULL;
+    len = 0;
+  }
 
   char operator[](std::size_t index) const { return buf[index]; }
-  bool operator==(const StringView &other) const;
-  bool operator<(const StringView &other) const;
 
   const char *startsWith(const char *target) const;
   const char *endsWith(const char *target) const;
 };
+
+inline bool operator<(const StringView &lhs, const StringView &rhs) {
+  std::size_t min_len =
+      (lhs.length() < rhs.length()) ? lhs.length() : rhs.length();
+  int cmp = std::strncmp(lhs.data(), rhs.data(), min_len);
+
+  if (cmp != 0) {
+    return cmp < 0;
+  }
+  return lhs.length() < rhs.length();
+}
+
+inline bool operator==(const StringView &lhs, const StringView &rhs) {
+  if (lhs.length() != rhs.length()) {
+    return false;
+  }
+
+  if (lhs.data() == rhs.data()) {
+    return true;
+  }
+
+  return std::strncmp(lhs.data(), rhs.data(), lhs.length()) == 0;
+}
+
+inline bool operator==(const StringView &lhs, const char *rhs) {
+  return lhs == StringView(rhs);
+}
+
+inline bool operator==(const char *lhs, const StringView &rhs) {
+  return StringView(lhs) == rhs;
+}
 
 inline std::ostream &operator<<(std::ostream &os, const StringView &sv) {
   if (sv.data()) {
