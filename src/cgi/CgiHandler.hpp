732a7f251cd22e6786df117f59ed785d85c5d03d
teathas <asmaber722@gmail.com>
2026-07-06 01:58:24 +0100

    update: reform CgiHandler

--- src/cgi/CgiHandler.hpp
+++ src/cgi/CgiHandler.hpp
@@ -3,33 +3,36 @@
 
 #include <string>
 #include <map>
+#include "../http/http_request.hpp"
+
+enum CgiState
+{
+	WRITING_BODY, READING_OUTPUT, CGI_DONE, CGI_ERROR
+};
 
 class CgiHandler
 {
 private:
-	int									pipe_in[2];
-	int									pipe_out[2];
-	pid_t									_pid;
-	std::string							_output;
-	std::string							_scriptPath;
-	std::string							_interpreter;
-	std::string							_body;
-	std::map<std::string, std::string>	_env;
+	int pipe_in[2];
+	int pipe_out[2];
+	pid_t pid;
 
-	void		childProcess();
-	char**		buildEnvp() const;
-	void		freeEnvp(char** envp, int size);
+	const char *body; //until it's parsed
+	size_t body_len;
+	size_t body_written;
+
+	std::string cgi_output;
+
+	CgiState state;
+	const HttpRequest* request;
+
+	CgiHandler(const CgiHandler& other);
+	CgiHandler& operator=(const CgiHandler& other);
 
 public:
-	CgiHandler(const std::string& scriptPath,
-			const std::string& interpreter,
-			const std::string& body,
-			const std::map<std::string, std::string>& env);
-
-	int				start();
-	bool			readChunk();
-	std::string		getOutput() const;
-	std::string		buildResponse(const std::string& cgiOutput);
+	CgiHandler();
+	CgiHandler(const HttpRequest* request,const char *body, size_t body_len);
+	~CgiHandler();
 };
 
 #endif
