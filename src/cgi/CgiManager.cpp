df4aca27ed48e279519d0712d1d71411a5d84d4c
teathas <asmaber722@gmail.com>
2026-07-11 07:33:00 +0100

    update: refactor CgiManager, add lookup function for cgi pipes fd

--- src/cgi/CgiManager.cpp
+++ src/cgi/CgiManager.cpp
@@ -1,44 +1,14 @@
 #include "CgiManager.hpp"
 #include <sstream>
 
-CgiManager::CgiManager(int& epoll_fd) : _epoll_fd(epoll_fd) {}
+CgiManager::CgiManager(int epoll_fd) : epoll_fd(epoll_fd) {}
 
-std::map<std::string, std::string> CgiManager::buildEnv(const Request& req, const SharedConfig& conf)
+CgiManager::owns(int fd) const
 {
-	std::map<std::string, std::string> env;
-
-	env["REQUEST_METHOD"] = req.getMethod();
-	env["CONTENT_TYPE"] = req.getContentType();
-	env["QUERY_STRING"] = req.getQuery();
-	env["SCRIPT_FILENAME"] = conf.root +  req.getPath();
-
-	std::ostringstream ss;
-	ss << req.getBody().size();
-	env["CONTENT_LENGTH"] = ss.str();
-
-	return (env);
-}
-
-std::string CgiManager::getExtension(const std::string& path)
-{
-	size_t pos = path.find_last_of('.');
-	if (pos == std::string::npos)
-		return ("");
-	std::string extension = path.substr(pos);
-	return (extension);
-}
-
-int CgiManager::handleRequest(const Request& req, const SharedConfig& conf, int client_fd)
-{
-	std::map<std::string, std::string> env = buildEnv(req, conf);
-	std::string interpreter = conf.cgi_pass[getExtension(req.getPath())];
-
-	CgiHandler cgi(req.getPath(), interpreter, req.getBody(), env);
-	return (cgi.start());
-}
-
-
-bool	CgiManager::isCgiFd(int fd)
-{
-	return (_cgiHandlers.count(fd));
+	for (std::vector<CgiHandler*>::size_type i = 0; i < handlers.size(); i++)
+	{
+		if (fd == handlers[i]->getReadFd() || fd == handlers[i]->getWriteFd())
+			return (true);
+	}
+	return (false);
 }
--- src/cgi/CgiManager.hpp
+++ src/cgi/CgiManager.hpp
@@ -1,28 +1,25 @@
 #ifndef CGIMANAGER_HPP
 #define CGIMANAGER_HPP
 
-#include "../config/shared_config.hpp"
-#include "Request.hpp"
 #include "CgiHandler.hpp"
-#include <map>
+#include <sys/epoll.h>
+#include "../config/shared_config.hpp"
+#include <vector>
 
 class CgiManager
 {
 private:
-	std::map<int, CgiHandler*>		_cgiHandlers;
-	std::map<int, int>				_cgiToClient;
-	int&							_epoll_fd;
-
-	
-	std::map<std::string, std::string>	buildEnv(const Request& req, const SharedConfig& conf);
-	std::string 						getExtension(const std::string& path);
+	const int epoll_fd;
+	std::vector<CgiHandler*> handlers;
 
 public:
-	CgiManager(int& epoll_fd);
+	CgiManager(int epoll_fd);
+	~CgiManager();
 
-	int									handleRequest(const Request& req, const SharedConfig& conf, int client_fd);
-	std::string							pipeReady(int pipe_fd);
-	bool								isCgiFd(int fd);
+	bool owns(int fd) const;
+
+private:
+
 };
 
 #endif
