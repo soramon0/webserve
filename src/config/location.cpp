c6ff6dcf38566d75b74b5c439b407ba963c5ba00
karimxload <sora@x-foundation.io>
2026-07-12 19:12:54 +0100

    fix: method seed should be uppercase

--- src/config/location.cpp
+++ src/config/location.cpp
@@ -6,12 +6,12 @@
 
 Location::Location() : path(), return_rule(NULL) {
   shared_config = new SharedConfig();
-  methods.push_back("get");
+  methods.push_back("GET");
 };
 
 Location::Location(const std::string &path) : path(path), return_rule(NULL) {
   shared_config = new SharedConfig();
-  methods.push_back("get");
+  methods.push_back("GET");
 };
 
 Location::~Location() {
